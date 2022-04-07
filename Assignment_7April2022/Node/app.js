7e50830e41e12a7922489dfd089d322fdb668cca
mithun139-WAL <96226840+mithun139-WAL@users.noreply.github.com>
2022-04-07 21:15:12 +0530

    Added files

--- Assignment_7April2022/Node/app.js
+++ Assignment_7April2022/Node/app.js
@@ -30,7 +30,7 @@ const getApiAndEmit = (socket) => {
     var seconds = date.getSeconds();
     var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
     return strTime;
